3b6d01fab034e0b2585bc1baf27104411e095567
earonesty <earonesty@3dc5f398-04f5-11de-aadd-1d6f329b7c06>
2009-03-12 12:25:20 +0000

    added fcgi support only builds if you have fcgi-devel insstalled uses SMX_INIT environment variable altered the way qscgi works, so it's a real header/lib that can be used (ie to implement alternate streams) smx main exe now just uses that library smx exe also should support SMX_INIT environment

---
+++ fcgi/smx-fcgi.cpp
@@ -0,0 +1,125 @@
+/*
+Copyright 1998-2005 Erik Aronesty. All rights reserved.
+
+        1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+        2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+	3. Original source code, information and documentation is available at www.smxlang.org
+
+THIS SOFTWARE IS PROVIDED 'AS IS' AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE SMX AUTHORS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+*/
+
+// psmx.cpp : Defines the entry point for the console application.
+//
+
+#ifdef unix
+#define NO_LOAD_SCHED
+#endif
+#define NO_LOAD_TSET
+
+#include "fcgi_stdio.h"
+
+#include "stdafx.h"
+
+#include "fcgi_config.h"
+
+#include "qstr.h"
+#include "qobj.h"
+#include "qctx.h"
+
+#include "opt.h"
+#include "libs.h"
+
+#include "qscgi.h"
+
+#ifdef _WIN32
+#include <process.h>
+#else
+extern char **environ;
+#endif
+void printerr(const char * msg, const char *arg=NULL);
+
+int main(int argc, char* argv[], char* envp[])
+{
+// Declare the variables needed
+	setarg(&argc, argv);
+	qEnvCGI env(NULL, stdout, 1024);
+	qCtxTmp global(&env);
+	LoadAllLibs(&global);
+
+	char *init;
+	if (init = getenv("SMX_INIT")) {
+		try {
+			global.Parse(init);
+		} catch (...) {
+			// log something here...
+		}
+	}
+
+	while (FCGI_Accept() >= 0) {
+		char * file = getenv("PATH_TRANSLATED");
+		if (file) {
+			FILE *fp = fopen(file, "r");
+			if (fp) {
+				qCtxTmp ctx(&global);
+				qEnvCGI env(fp, stdout, 1024);
+				
+				ctx.SetEnv(&env);
+				env.SetSessionCtx(&ctx);
+
+				qStrBuf out;
+
+				ctx.ParseTry(&env, &out);
+				env.PutS("Content-Length: ");
+				env.PutN(out.Length());
+				env.PutC('\n');
+				env.PrintHeaders();
+				env.PutC('\n');
+				env.PutS(out);
+
+				fclose(fp);
+			} else {
+				printerr("Failed to open file: %s", file);
+			}
+		} else {
+			printerr("PATH_TRANSLATED not set");
+		}
+		FCGI_Finish();
+        }
+	global.Clear();
+	return 0;
+}
+
+void printerr(const char *msg, const char *arg) {
+        puts("Content-Type: text/html\n\n");
+	printf(msg, arg);
+	printf("\n\n");	
+}
+
+void dumpenv(const char * msg) {
+	char **envp = environ;
+	puts("Status: 500\n");
+	puts("Content-Type: text/html\n\n");
+	printf("%s:<p><pre>\n",msg);
+	for ( ; *envp != NULL; envp++) {
+		printf("%s\n", *envp);
+	}
+	printf("</pre>\n");
+}
+
+qEnvCGI::qEnvCGI(FILE *script, FILE *out, int newBufSize)
+{
+        myIn = script;
+        myOut = out;
+        myBufSize = newBufSize;
+        myHeaders=NULL;
+}
+
+void qEnvCGI::PutS(const char *s)                {if(s) fputs(s, myOut);}
+void qEnvCGI::PutS(CStr &s)                      {fwrite(s.Data(), 1, s.Length(), myOut);}
+void qEnvCGI::PutS(const char *s, int n) {if (s) fwrite(s, 1, n, myOut);}
+void qEnvCGI::PutC(char c)                       {fputc(c, myOut);}
+char qEnvCGI::GetC()      {return fgetc(myIn);}
+CStr qEnvCGI::GetS()  {CStr tmp(myBufSize); tmp.Grow(fread(tmp.GetBuffer(), 1, myBufSize, myIn)); return tmp;}
